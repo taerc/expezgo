ee52f00a09a2559c88e31afb0d91a345233c456e
wangfangming <wangfangming@airia.cn>
2023-08-23 11:12:04 +0800

    AUTO-MSG

--- research/exp_19_ent_hook.go
+++ research/exp_19_ent_hook.go
@@ -3,6 +3,8 @@ package main
 import (
 	"context"
 	"expezgo/modules/ent"
+	"expezgo/modules/ent/hook"
+	"expezgo/modules/ent/user"
 	"fmt"
 
 	"entgo.io/ent/dialect"
@@ -23,16 +25,61 @@ func DBD() *ent.Client {
 	c := ent.NewClient(ent.Driver(dialect.DebugWithContext(drv, func(ctx context.Context, a ...any) {
 	})))
 
+	// c.Use(func(next ent.Mutator) ent.Mutator {
+	// 	return ent.MutateFunc(func(ctx context.Context, m ent.Mutation) (ent.Value, error) {
+	// 		fmt.Println("mutate", m.Type())
+	// 		if e := m.SetField("created_at", ezgo.GetUnixTimeStamp()); e != nil {
+	// 			fmt.Println("set time error ", e.Error())
+	// 		}
+	// 		return next.Mutate(ctx, m)
+	// 	})
+	// })
+
+	c.Use(hook.On(updateTimestamp(), ent.OpUpdate|ent.OpUpdateOne))
 	return c
 }
 
-func AddUser(ctx context.Context, c *ent.Client) {
-	_, e := c.Debug().User.Create().SetUsername("wangfangming").SetPassword("123456").
-		SetRealname("wfm").
-		SetLoginLat(decimal.Decimal{}).Save(ctx)
-	if e != nil {
-		fmt.Println(e.Error())
+// func updateTimestamp(next ent.Mutator) ent.Mutator {
+// 	return ent.MutateFunc(func(ctx context.Context, m ent.Mutation) (ent.Value, error) {
+// 		if e := m.SetField("created_at", ezgo.GetUnixTimeStamp()); e != nil {
+// 			fmt.Println("set time error ", e.Error())
+// 		}
+// 		return next.Mutate(ctx, m)
+// 	})
+// }
+
+func updateTimestamp() func(next ent.Mutator) ent.Mutator {
+	return func(next ent.Mutator) ent.Mutator {
+		return ent.MutateFunc(func(ctx context.Context, m ent.Mutation) (ent.Value, error) {
+			if e := m.SetField("updated_at", ezgo.GetUnixTimeStamp()); e != nil {
+				fmt.Println("set time error ", e.Error())
+			}
+			return next.Mutate(ctx, m)
+		})
 	}
+}
+
+func AddUser(ctx context.Context, c *ent.Client) {
+
+	if v, err := decimal.NewFromString("101.111111"); err == nil {
+		_, e := c.Debug().User.Create().SetUsername("wangfangming").SetPassword("123456").
+			SetRealname("wfm").
+			SetLoginLng(v).
+			SetLoginLat(v).Save(ctx)
+		if e != nil {
+			fmt.Println(e.Error())
+		}
+	}
+}
+
+func UpdateUser(ctx context.Context, c *ent.Client) {
+
+	if n, e := c.User.Update().Where(user.Username("wangfangming")).SetUsername("fangming").Save(ctx); e != nil {
+		fmt.Println(e.Error())
+	} else {
+		fmt.Println(n)
+	}
+
 }
 
 func main() {
@@ -46,6 +93,7 @@ func main() {
 		Charset:       "utf8mb4",
 		ParseTime:     "true",
 		Loc:           "Local",
+		MaxLifeTime:   "1h",
 	}
 
 	//ezgo.WithComponentMySQL("mysql", &c)
@@ -60,6 +108,8 @@ func main() {
 	//client.Debug()
 	ctx := context.Background()
 
-	AddUser(ctx, DBD())
+	// AddUser(ctx, DBD())
+
+	UpdateUser(ctx, DBD())
 
 }
