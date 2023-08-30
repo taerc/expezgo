8cc2b33511f268a4dfae31f760c8e443d9995a4f
wangfangming <wangfangming@airia.cn>
2023-08-30 20:12:04 +0800

    AUTO-MSG

--- modules/drone/t_image.go
+++ modules/drone/t_image.go
@@ -13,7 +13,24 @@ type TImage struct {
 }
 
 func (TImage) Fields() []ent.Field {
-	return []ent.Field{field.Int("id"), field.String("imgName"), field.Int32("imgWidth"), field.Int32("imgHeight"), field.Float("longtitude"), field.Float("latitude"), field.Float32("altitude"), field.Time("photoTime"), field.Int8("imgType"), field.String("thumbnailId"), field.String("mediaId"), field.String("comprId"), field.Int8("is_ai_check"), field.Int8("is_ok"), field.Int8("is_check"), field.Time("checkTime"), field.String("xmlPath"), field.Time("createTime")}
+	return []ent.Field{field.Int("id"),
+ field.String("imgName"),
+ field.Int32("imgWidth"),
+ field.Int32("imgHeight"),
+ field.Float("longtitude"),
+ field.Float("latitude"),
+ field.Float32("altitude"),
+ field.Time("photoTime"),
+ field.Int8("imgType"),
+ field.String("thumbnailId"),
+ field.String("mediaId"),
+ field.String("comprId"),
+ field.Int8("is_ai_check"),
+ field.Int8("is_ok"),
+ field.Int8("is_check"),
+ field.Time("checkTime"),
+ field.String("xmlPath"),
+ field.Time("createTime")}
 }
 func (TImage) Edges() []ent.Edge {
 	return nil
