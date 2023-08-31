503872e5c99527ba31e31119967d3bf8e703a55d
wangfangming <wangfangming@airia.cn>
2023-08-31 20:12:05 +0800

    AUTO-MSG

--- research/exp_19_ent_hook.go
+++ research/exp_19_ent_hook.go
@@ -8,9 +8,25 @@ import (
 	"fmt"
 
 	"entgo.io/ent/dialect"
+	entsql "entgo.io/ent/dialect/sql"
 	"github.com/taerc/ezgo"
 	"github.com/taerc/ezgo/conf"
 )
+
+func InitDB() *ent.Client {
+
+	drv, e := entsql.Open("mysql", "wp:wORd@2314@tcp(127.0.0.1:3306)/buckets?parseTime=True")
+	if e != nil {
+		fmt.Println(e)
+		return nil
+	}
+	DB := ent.NewClient(ent.Driver(dialect.DebugWithContext(drv, func(ctx context.Context, v ...any) {
+		fmt.Println(v)
+		data := fmt.Sprintf("%v", v)
+		fmt.Printf("=== %s", data)
+	})))
+	return DB
+}
 
 func DBD() *ent.Client {
 
@@ -36,23 +52,12 @@ func DBD() *ent.Client {
 
 	c.Intercept(ent.InterceptFunc(func(q ent.Querier) ent.Querier {
 		return ent.QuerierFunc(func(ctx context.Context, query ent.Query) (ent.Value, error) {
-			fmt.Println("query ..... ....")
-			return q.Query(ctx, query)
+			v, e := q.Query(ctx, query)
+			return v, e
 		})
 	}))
 	c.Use(hook.On(updateTimestamp(), ent.OpUpdate|ent.OpUpdateOne))
-
-	// ent.InterceptFunc(func(next ent.Querier) ent.Querier {
-	// 	return ent.QuerierFunc(func(ctx context.Context, query ent.Query) (ent.Value, error) {
-	// 		// Do something before the query execution.
-	// 		fmt.Println("this is BEFER ..")
-	// 		value, err := next.Query(ctx, query)
-	// 		// Do something after the query execution.
-	// 		fmt.Println("this is END ...")
-	// 		return value, err
-	// 	})
-	// })
-	return c
+	return c.Debug()
 }
 
 // func updateTimestamp(next ent.Mutator) ent.Mutator {
@@ -77,7 +82,7 @@ func updateTimestamp() func(next ent.Mutator) ent.Mutator {
 
 func AddUser(ctx context.Context, c *ent.Client) {
 
-	_, e := c.Debug().User.Query().All(ctx)
+	_, e := c.User.Query().All(ctx)
 	if e != nil {
 		fmt.Println(e.Error())
 	}
@@ -120,7 +125,7 @@ func main() {
 
 	ctx := context.Background()
 
-	AddUser(ctx, DBD())
+	AddUser(ctx, InitDB())
 
 	// UpdateUser(ctx, DBD())
 
