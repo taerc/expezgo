dfc0d5986d7a78b367275a0df157ab19c5d32de3
wfm <wfm@airia.cn>
2023-09-28 17:06:05 +0800

    AUTO-MSG

--- research/exp_25_byteorder.go
+++ research/exp_25_byteorder.go
@@ -6,6 +6,41 @@ import (
 	"unsafe"
 )
 
+type GSFrameCodecConfig struct {
+	StartDelimiterOffset int
+	SendSequenceOffset   int
+	RecvSequenceOffset   int
+	FrameTypeOffset      int
+	DataLengthOffset     int
+	DataOffset           int
+	EndDelimiterOffset   int
+	FrameDelimiter       uint16
+}
+
+func GetGSFrameCodecConfig() *GSFrameCodecConfig {
+	return &GSFrameCodecConfig{
+		StartDelimiterOffset: 0,
+		SendSequenceOffset:   2,
+		RecvSequenceOffset:   10,
+		FrameTypeOffset:      18,
+		DataLengthOffset:     19,
+		DataOffset:           15,
+		FrameDelimiter:       0xEB90,
+	}
+}
+
+type GSFrameCodec struct {
+	StartTag uint16
+	SendSeq  uint64 // inc
+	RecvSeq  uint64 // inc
+	Type     byte   // 0x00 request 0x01 response
+	Length   uint32
+	Data     []byte
+	EndTag   uint16
+
+	config *GSFrameCodecConfig
+}
+
 func main() {
 
 	var data uint64 = 0x1122334455667788
@@ -30,7 +65,16 @@ func main() {
 	fmt.Printf("==%02x\n", b)
 	b = *(*byte)(unsafe.Pointer(unsafe.Add(z, 7)))
 	fmt.Printf("==%02x\n", b)
+	var c byte = 0
+	fmt.Println(unsafe.Sizeof(c))
 
-	unsafe.Sizeof(byte)
+	x := &GSFrameCodec{}
+
+	fmt.Println("offset startTag :", unsafe.Offsetof(x.StartTag))
+	fmt.Println("offset sendSeq:", unsafe.Offsetof(x.SendSeq))
+	fmt.Println("offset recvSeq:", unsafe.Offsetof(x.RecvSeq))
+	fmt.Println("offset type:", unsafe.Offsetof(x.Type))
+	fmt.Println("offset recvSeq:", unsafe.Offsetof(x.Length))
+	fmt.Println("offset recvSeq:", unsafe.Offsetof(x.EndTag))
 
 }
